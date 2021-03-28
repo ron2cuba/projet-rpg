6c9bd5d948a10dd8d1d824f4af4ec06d085187e9
ron2cuba <ronan.lenouvel@gmail.com>
2021-03-28 19:24:01 +0200

    Classe Hero

---
+++ js/Knight.js
@@ -0,0 +1,14 @@
+export default class Knight {
+    constructor(){
+        this.name = 'Valrous';
+        this.health = 100;
+        this.weapon = 'Tiny Sword';
+        this.pack = [];
+    }
+    getHealth(){
+        console.log('health');
+    }
+    draw(){
+    }
+    
+}
