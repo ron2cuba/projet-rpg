9d75a76887a5dad99c35d0651ecfb709cb7c44bf
ron2cuba <ronan.lenouvel@gmail.com>
2021-03-29 15:40:33 +0200

    reset KeyHandler.js

---
+++ js/KeyHandler.js
@@ -0,0 +1,12 @@
+
+export default class KeyHandler{
+
+ 
+
+}
+// move(keyPressed){
+//     if(keyPressed == true){
+//         yPos+=10;
+//         ctx.drawImage(image, 0, 0, 34, 50, this.position.x, yPos, 34, 50);
+//     }
+//  }
