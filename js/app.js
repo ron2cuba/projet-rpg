48a1abcd2838eb09dad6410170c9980d1d7cacf7
ron2cuba <ronan.lenouvel@gmail.com>
2021-03-29 13:48:33 +0200

    feat: display/hide status infos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 import Knight from './Knight.js';
+import KeyHandler from './KeyHandler.js';
 
-const knight = new Knight('toto');
+const knight = new Knight();
+// const keyHandler = new KeyHandler();
 
 console.log(knight);
 
@@ -8,6 +10,13 @@ console.log(knight);
  * Var
  */
 const body = document.querySelector('body');
+let infos = document.createElement('div');
+let status = document.createElement('div');
+let pack = document.createElement('div');
+let map = document.createElement('canvas');
+let initialPosition = {x: 20,y: 150}
+
+
 /**
  * Canvas
  */
@@ -20,70 +29,77 @@ canvas.style.border ="1px solid";
 let ctx = canvas.getContext('2d');
 let image = new Image();
 image.src = '../img/chara.png'
-ctx.drawImage(image, 0, 0, 34, 50, 20, 150, 34, 50);
+ctx.drawImage(image, 0, 0, 34, 50, initialPosition.x, initialPosition.y, 34, 50);
 
 /**
  * Infos, status, map
  */
 
 // infos
-let infos = document.createElement('div');
 infos.classList.add('infos');
 body.appendChild(infos);
 
 // status
-let status = document.createElement('div');
 status.classList.add('status');
 status.width = '1fr';
 status.height = '100px';
 infos.appendChild(status);
 
-// nom
+// nom health lvl exp
 status.innerHTML = `
     <div class="status-content">Name : <span class="name">${knight.name}</span></div>
-    <div class="status-content">Health : <span class="health-content">${knight.health}</span></div>
+    <div class="status-content">Health : <span class="number">${knight.health}</span></div>
     <div class="status-content">Weapon : <span class="name">${knight.weapon}</span></div>
+    <div class="status-content">Lvl : <span class="number">${knight.lvl}</span></div>
+    <div class="status-content">Exp : <span class="number">${knight.exp}</span></div>
 `;
 
 // empty
-let map = document.createElement('canvas');
 map.setAttribute('id', 'mini-map');
 map.width = '1fr';
 map.height = '100px';
 infos.appendChild(map);
 
 // pack
-let pack = document.createElement('div');
 pack.classList.add('pack');
 infos.appendChild(pack);
 pack.width = '1fr';
 
 
 /**
- * Move
+ * diusplay Inventory
  */
-let rightPressed = false;
-let leftPressed = false;
-let upPressed = false;
-let downPressed = false;
+ function pressKeyDown(e){
+        if(e.key == "i"|| e.key == "I"){
+            console.log('yep');
+            if(infos.classList.contains('hide') == false){   
+                infos.classList.add('hide');
+            } else {
+                infos.classList.remove('hide');
+            }
+        }
+     }
 
-document.addEventListener('keydown', keyDownHanlder, false);
+document.addEventListener('keydown', pressKeyDown, false);
 
-function keyDownHanlder(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
-        console.log('right pressed');
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
-        console.log('left pressed');
-    }
-    else if(e.key =="Up" || e.key == "ArrowUp"){
-        console.log('up pressed');
-        upPressed = true;
-    }
-    else if(e.key =="Down" || e.key == "ArrowDown"){
-        console.log('down pressed');
-        upPressed = true;
-    }
-}
+// function keyDownHanlder(e) {
+//     if(e.key == "Right" || e.key == "ArrowRight") {
+//         rightPressed = true;
+//     }
+//     else if(e.key == "Left" || e.key == "ArrowLeft") {
+//         leftPressed = true;
+//     }
+//     else if(e.key =="Up" || e.key == "ArrowUp"){
+//         upPressed = true;
+//     }
+//     else if(e.key =="Down" || e.key == "ArrowDown"){
+//         downPressed = true;
+//     }
+//     else if(e.key == "&"){
+//         inventory = true;
+//         console.log(inventory);
+//     }
+// }
+
+
+// apparition inventory
